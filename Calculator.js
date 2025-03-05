72e74f0de9ad14b568b8e5f5b3acaf18cf8252b2
Yash raval <123889939+Yashraval366@users.noreply.github.com>
2025-03-05 18:41:22 +0530

    first commit

---
+++ Calculator.js
@@ -0,0 +1,24 @@
+const a = 10;
+const b = 20;
+
+
+function add(a, b){
+    return a + b;    
+}
+
+function sub(a, b){
+    return a - b;
+}
+
+function multi(a, b){
+    return a * b;
+}
+
+function divi(a, b){
+    return a/b;
+}
+
+console.log(add(a, b));
+console.log(sub(a, b));
+console.log(multi(a, b));
+console.log(divi(a, b));
